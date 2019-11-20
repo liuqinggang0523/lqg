4606a2513dfa9dbd09b1e2eec6a90ca41fe82f18
liuqinggang <1542574711@qq.com>
2019-11-20 17:27:25 +0800

    优化js,让user.js更加简单，只获取数据，不对dom元素操作

--- web_back/js/user.js
+++ web_back/js/user.js
@@ -1,35 +1,27 @@
- user = {
-   login: function(userName, password) {
-     $.post('http://localhost:8000/admin/login', {
-         user_name: userName,
-         password: password
-       },
-       function(res) {
-         // console.log(res);
-         if (res.code === 200) {
-           window.location.href = 'index.html';
-         } else {
-           $('#myModal').modal('show')
-           $('#tankuang').text('账号或密码错误')
-         }
-       })
-   },
+// 把地址公用的信息单独拿出来，以全局变量来保存
+var Url = 'http://localhost:8000'
+var user = {
+  // 此接口有两个参数，所以需要设置参数接受
+  login: function(userName, password, callback) {
+    $.post(Url + '/admin/login', {
+        user_name: userName,
+        password: password
+      },
+      function(res) {
+        callback(res)
+      })
+  },
 
-   logout: function() {
-     $.post('http://localhost:8000/admin/logout', function(res) {
-       console.log(res);
-       if (res.code === 200) {
-         location.href = 'login.html'
-       }
-     })
-   },
+  logout: function(callback) {
+    $.post(Url + '/admin/logout', function(res) {
+      callback(res)
+    })
+  },
 
-   getInfo: function() {
-     $.get('http://localhost:8000/admin/getuser',
-       function(res) {
-         console.log(res);
-         $('#username').text(res.data.nickname);
-         $('#userimg').prop('src', res.data.user_pic);
-       })
-   }
- }
+  getInfo: function(callback) {
+    $.get(Url + '/admin/getuser',
+      function(res) {
+        callback(res)
+      })
+  }
+}
